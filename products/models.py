d25b3566fb2153b6a61891df3b74583994d07896
Thorsten Roeske <78468336+troeske@users.noreply.github.com>
2024-10-23 20:45:49 +0200

    fix: product model

--- products/models.py
+++ products/models.py
@@ -7,7 +7,7 @@ from datetime import date
 class ProductCategory(models.Model):
     name = models.CharField(max_length=250)
     friendly_name = models.CharField(max_length=250, null=True, blank=True)
-    description = models.TextField(blank=True)
+    description = models.TextField(null=True, blank=True)
     hero_image = CloudinaryField('image', default='placeholder')
 
     class Meta:
@@ -23,7 +23,7 @@ class ProductCategory(models.Model):
 
 class ProductType(models.Model):
     name = models.CharField(max_length=100, default='')
-    description = models.TextField(blank=True)
+    description = models.TextField(null=True, blank=True)
     hero_image = CloudinaryField('image', default='placeholder')
 
     def __str__(self):
@@ -32,8 +32,8 @@ class ProductType(models.Model):
 
 class ProductBrand(models.Model):
     name = models.CharField(max_length=100, default='')
-    description = models.TextField(blank=True)
-    link = models.URLField()
+    description = models.TextField(null=True, blank=True)
+    link = models.URLField(null=True, blank=True)
     link_text = models.CharField(max_length=100, default='Visit Website')
     alt_text = models.CharField(max_length=100, default='logo')
     logo = CloudinaryField('image', default='placeholder')
@@ -58,9 +58,9 @@ class CaskType(models.Model):
 
 class Bottler(models.Model):
     name = models.CharField(max_length=100, default='')
-    description = models.TextField(blank=True)
-    link = models.URLField()
-    link_text = models.CharField(max_length=100, default='Visit Website')
+    description = models.TextField(null=True, blank=True)
+    link = models.URLField(null=True, blank=True)
+    link_text = models.CharField(max_length=100, default='Visit Website', null=True, blank=True)
     alt_text = models.CharField(max_length=100, default='logo')
     logo = CloudinaryField('image', default='placeholder')
     
@@ -77,12 +77,12 @@ class Product(models.Model):
     region = models.CharField(max_length=250, default='', blank=True)
     brand = models.ForeignKey(ProductBrand, null=True, blank=True, on_delete=models.SET_NULL, related_name='product_brand')
     bottler = models.ForeignKey(Bottler, null=True, blank=True, on_delete=models.SET_NULL, related_name='product_bottler')
-    cask_type = models.ForeignKey(CaskType, on_delete=models.CASCADE, related_name='cask_type')
-    abv = models.DecimalField(max_digits=4, decimal_places=2)
-    age = models.IntegerField(blank=True)
-    vintage = models.IntegerField(blank=True)
+    cask_type = models.ForeignKey(CaskType, null=True, blank=True, on_delete=models.SET_NULL, related_name='cask_type')
+    abv = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+    age = models.IntegerField(null=True, blank=True)
+    vintage = models.IntegerField(null=True, blank=True)
     size = models.ForeignKey(ProductSize, null=True, blank=True, on_delete=models.SET_NULL, related_name='product_size')
-    rating = models.DecimalField(max_digits=6, decimal_places=2, blank=True)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     
     class Meta:
         ordering = ["name", "type", "product_category"]
@@ -110,12 +110,12 @@ class ProductReview(models.Model):
 
 class ProductTastingNote(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    origin = models.TextField()
-    link = models.URLField()
-    nose = models.TextField()
-    palate = models.TextField()
-    finish = models.TextField()
-    overall = models.TextField()
+    origin = models.TextField(null=True, blank=True)
+    link = models.URLField(null=True, blank=True)
+    nose = models.TextField(null=True, blank=True)
+    palate = models.TextField(null=True, blank=True)
+    finish = models.TextField(null=True, blank=True)
+    overall = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.product.name} | {self.origin}"
