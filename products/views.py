bdcb4f908b94f71336740892d8537b11355ac9e6
Thorsten Roeske <78468336+troeske@users.noreply.github.com>
2024-10-24 11:36:00 +0200

    add: category search

--- products/views.py
+++ products/views.py
@@ -10,7 +10,8 @@ def all_products(request):
     products = Product.objects.all()
     
     query = None
-    
+    category = None
+
     if request.GET:
         if 'q' in request.GET:
             query = request.GET['q']
@@ -21,11 +22,19 @@ def all_products(request):
             # normally filter uses AND; with Q we can use OR!!!
             query = Q(name__icontains=query) | Q(description__icontains=query)    
             products = products.filter(query)
-            
+        
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            # filter using fk product_category pointing to ProductCategory
+            # and __name to get the name field of the ProductCategory
+            products = products.filter(product_category__name__in=categories)
+            categories = ProductCategory.objects.filter(name__in=categories)
+             
             
     context = {
         'products': products,
         'serch_term': query,
+        'current_categories': categories,
     }
     
     return render(request, "products/products.html", context)
@@ -33,11 +42,8 @@ def all_products(request):
 def product_details(request, gtin):
     """ A view that displays a single product's details. """
     
-    
     product = get_object_or_404(Product, pk=gtin)
-    print("GTIN: ", gtin)
-    print("Product: ", product)
-    
+
     context = {
         'product': product,
     }
