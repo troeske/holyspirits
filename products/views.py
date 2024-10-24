b1114977036aab5b08b0214652a412732262f961
Thorsten Roeske <78468336+troeske@users.noreply.github.com>
2024-10-24 11:07:38 +0200

    add: product search

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import *
 
 # Create your views here.
@@ -7,8 +9,23 @@ def all_products(request):
     
     products = Product.objects.all()
     
+    query = None
+    
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            # normally filter uses AND; with Q we can use OR!!!
+            query = Q(name__icontains=query) | Q(description__icontains=query)    
+            products = products.filter(query)
+            
+            
     context = {
         'products': products,
+        'serch_term': query,
     }
     
     return render(request, "products/products.html", context)
@@ -16,7 +33,10 @@ def all_products(request):
 def product_details(request, gtin):
     """ A view that displays a single product's details. """
     
+    
     product = get_object_or_404(Product, pk=gtin)
+    print("GTIN: ", gtin)
+    print("Product: ", product)
     
     context = {
         'product': product,
