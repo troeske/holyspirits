b817e53473c74e3931bdb27eeead4bb89890d8a6
Thorsten Roeske <78468336+troeske@users.noreply.github.com>
2024-10-24 14:18:28 +0200

    add: js for sort box; btt button and js

--- products/views.py
+++ products/views.py
@@ -36,10 +36,14 @@ def all_products(request):
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
             sort = sortkey
+            
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-                
+            
+            if sortkey == 'productcategory':
+                sortkey = 'product_category__name'    
+            
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
