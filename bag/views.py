a18e4b946691534b3ec9c9ed74e063369a1e2345
Thorsten Roeske <78468336+troeske@users.noreply.github.com>
2024-10-25 12:53:32 +0200

    add: show bag in success toast

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 from products.models import Product
 
@@ -12,7 +12,7 @@ def view_bag(request):
 def add_to_bag(request, item_gtin):
     """ Add a quantity of the specified product to the shopping bag """
     
-    product = Product.objects.get(gtin=item_gtin)
+    product = get_object_or_404(Product, gtin=item_gtin)
     
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -20,6 +20,7 @@ def add_to_bag(request, item_gtin):
     
     if item_gtin in list(bag.keys()):
         bag[item_gtin] += quantity
+        messages.success(request, f'Added {product.name} to your shopping bag')
     else:
         bag[item_gtin] = quantity
         messages.success(request, f'Added {product.name} to your shopping bag')
@@ -49,6 +50,8 @@ def remove_from_bag(request, item_gtin):
     """ Ajust a quantity of the specified product to the shopping bag """
     
     try:
+        product = get_object_or_404(Product, gtin=item_gtin)
+        
         bag = request.session.get('bag', {})
         
         print(bag)
@@ -61,7 +64,6 @@ def remove_from_bag(request, item_gtin):
         return HttpResponse(status=200)
     
     except Exception as e:
-        error_message = f"the following error occurred in remove_from_bag: {e}."
-        print(error_message)
-        print(type(e))
+        error_message = f"Error removing item: {product} | {e}."
+        messages.error(request, error_message)
         return HttpResponse(status=500)
