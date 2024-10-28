4b855fcf31555ef0b5fca88294ca6b7cadc9ea8e
Thorsten Roeske <78468336+troeske@users.noreply.github.com>
2024-10-28 14:44:35 +0100

    add: strip card element and js file

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
     
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51QEq3IRsi2MgbMpuhlcJncrdjJRqlcVI9Ul1QuD206zyAS53S0ZTn09dc0dvwkQkRNDwnhsNw3mKrJrPClmdDpSJ005Bvo2AWX',
+        'client_secret': 'test client secret',
         }
     
     return render(request, template, context)
