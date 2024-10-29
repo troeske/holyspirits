4dd10544778907626d749e7a132a1264c7e9ac3c
Thorsten Roeske <78468336+troeske@users.noreply.github.com>
2024-10-29 15:48:20 +0100

    add: stripe payment flow complete without testing webhook as we did not deploy yet

--- checkout/views.py
+++ checkout/views.py
@@ -16,7 +16,8 @@ import json
 @require_POST
 def cache_checkout_data(request):
     """ A view to cache the checkout data and add the payment intent to the cache """
-    try:
+    
+    try:      
         pid = request.POST.get('client_secret').split('_secret')[0]
         stripe.api_key = settings.STRIPE_SECRET_KEY
         stripe.PaymentIntent.modify(pid, metadata={
@@ -24,9 +25,9 @@ def cache_checkout_data(request):
             'save_info': request.POST.get('save_info'),
             'username': request.user,
         })
-        
+
         return HttpResponse(status=200)
-    
+
     except Exception as e:
         messages.error(request, 'Sorry, your payment cannot be processed right now. Please try again later.')
         return HttpResponse(content=e, status=400)
@@ -54,8 +55,14 @@ def checkout(request):
         order_form = OrderForm(form_data)
         
         if order_form.is_valid():
-            order = order_form.save()
-            
+            order = order_form.save(commit=False)
+
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+
+            order.save()
+             
             for gtin, item_data in bag.items():
                 try:
                     product = Product.objects.get(pk=gtin)
